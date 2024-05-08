1374043f9d179841d43297f28260420007532593
8uziak <matbuziak@gmail.com>
2024-05-08 14:28:52 +0200

    added custom cover feature, changed gui.py to proper OOP

--- src/manga_kindle/gui.py
+++ src/manga_kindle/gui.py
@@ -8,17 +8,26 @@ class Gui(tk.Tk):
     def __init__(self):
         super().__init__()
         self.gui_functionality = GuiFunctionality()
+        self.main = CbzFilesToOneCbz()
+        
+        self.window()
+        self.background()
+        self.button_custom_cover()
+        self.button_add_directory()
+        self.button_add_file()
+        self.button_convert()
 
+    def window(self):
         self.title('mangaKindle')
         self.geometry("700x700")
 
-        iconPhoto = PhotoImage(file = self.gui_functionality.relative_to_assets("icon.png"))
-        self.iconphoto(False, iconPhoto)
+        self.iconPhoto = PhotoImage(file = self.gui_functionality.relative_to_assets("icon.png"))
+        self.iconphoto(False, self.iconPhoto)
         
         self.configure(bg = "#FFFFFF")
-        self.main = CbzFilesToOneCbz()
 
-        canvas = Canvas(
+    def background(self):
+        self.canvas = Canvas(
             self,
             bg = "#FFFFFF",
             height = 700,
@@ -28,88 +37,126 @@ class Gui(tk.Tk):
             relief = "ridge"
         )
 
-        canvas.place(x = 0, y = 0)
-        canvas.create_rectangle(
+        self.canvas.place(x = 0, y = 0)
+        self.canvas.create_rectangle(
             0.0,
             0.0,
             700.0,
             700.0,
             fill="#434343",
             outline="")
+    
+    def button_custom_cover(self):
 
-        button_image_1 = PhotoImage(
-            file=self.gui_functionality.relative_to_assets("button_1.png"))
-        button_1 = Button(
-            image=button_image_1,
+        self.button_image_0 = PhotoImage(
+            file=self.gui_functionality.relative_to_assets("button_0.png"))
+        self.button_0 = Button(
+            image=self.button_image_0,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: [entry_1.delete(0,"end"), entry_1.insert(0, self.gui_functionality.browseDirectory())],
+            command=lambda: [self.entry_0.delete(0,"end"), self.entry_0.insert(0, self.gui_functionality.browseFilePlusDirectory())],
             relief="flat"
         )
-        button_1.place(
+        self.button_0.place(
+            x=92.0,
+            y=139.0,
+            width=515.0,
+            height=71.0
+        )
+
+        self.entry_image_0 = PhotoImage(
+            file=self.gui_functionality.relative_to_assets("entry.png"))
+        self.entry_bg_3 = self.canvas.create_image(
+            348.5,
+            238.5,
+            image=self.entry_image_0
+        )
+        self.entry_0 = Entry(
+            bd=0,
+            bg="#4A4A4A",
+            fg="#000716",
+            highlightthickness=0
+        )
+        self.entry_0.place(
+            x=106.0,
+            y=219.0,
+            width=485.0,
+            height=37.0
+        )
+        
+    def button_add_directory(self):
+        self.button_image_1 = PhotoImage(
+            file=self.gui_functionality.relative_to_assets("button_1.png"))
+        self.button_1 = Button(
+            image=self.button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: [self.entry_1.delete(0,"end"), self.entry_1.insert(0, self.gui_functionality.browseDirectory())],
+            relief="flat"
+        )
+        self.button_1.place(
             x=92.0,
             y=279.0,
             width=515.0,
             height=71.0
         )
-
-
-        entry_image_1 = PhotoImage(
+        self.entry_image_1 = PhotoImage(
             file=self.gui_functionality.relative_to_assets("entry.png"))
-        entry_bg_1 = canvas.create_image(
+        self.entry_bg_1 = self.canvas.create_image(
             348.5,
             378.5,
-            image=entry_image_1
+            image=self.entry_image_1
         )
-        entry_1 = Entry(
+        self.entry_1 = Entry(
             bd=0,
             bg="#4A4A4A",
             fg="white",
             highlightthickness=0
         )
-        entry_1.place(
+        self.entry_1.place(
             x=106.0,
             y=359.0,
             width=485.0,
             height=37.0
         )
 
-        button_image_2 = PhotoImage(
+    def button_add_file(self):
+        self.button_image_2 = PhotoImage(
             file=self.gui_functionality.relative_to_assets("button_2.png"))
-        button_2 = Button(
-            image=button_image_2,
+        self.button_2 = Button(
+            image=self.button_image_2,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: [entry_2.delete(0,"end"), entry_2.insert(0, self.gui_functionality.browseFiles())],
+            command=lambda: [self.entry_2.delete(0,"end"), self.entry_2.insert(0, self.gui_functionality.browseFiles())],
             relief="flat"
         )
-        button_2.place(
+        self.button_2.place(
             x=91.0,
             y=419.0,
             width=515.0,
             height=71.0
         )
-
-        entry_image_2 = PhotoImage(
+        self.entry_image_2 = PhotoImage(
             file=self.gui_functionality.relative_to_assets("entry.png"))
-        entry_bg_2 = canvas.create_image(
+        self.entry_bg_2 = self.canvas.create_image(
             348.5,
             518.5,
-            image=entry_image_2
+            image=self.entry_image_2
         )
-        entry_2 = Entry(
+        self.entry_2 = Entry(
             bd=0,
             bg="#4A4A4A",
             fg="white",
             highlightthickness=0
         )
-        entry_2.place(
+        self.entry_2.place(
             x=106.0,
             y=499.0,
             width=485.0,
             height=37.0
         )             
 
+    def button_convert(self):
         button_image_3 = PhotoImage(
             file=self.gui_functionality.relative_to_assets("button_3.png"))
         button_3 = Button(
@@ -117,8 +164,9 @@ class Gui(tk.Tk):
             borderwidth=0,
             highlightthickness=0,
             command=lambda: self.convert(
-                entry_1.get() if entry_1.get() != '' else None, 
-                entry_2.get() if entry_2.get() != '' else None
+                self.entry_0.get() if self.entry_0.get() != '' else None,
+                self.entry_1.get() if self.entry_1.get() != '' else None, 
+                self.entry_2.get() if self.entry_2.get() != '' else None
                 ),
             relief="flat"
         )
@@ -131,12 +179,12 @@ class Gui(tk.Tk):
         self.resizable(False, False)
         self.mainloop()
     
-    def convert(self, entry_add_directory, entry_add_file):
+    def convert(self, entry_custom_cover, entry_add_directory, entry_add_file):
         try:
-            self.main.rename_cbz_to_zip(entry_add_directory, entry_add_file)
+            self.main.rename_cbz_to_zip(entry_add_directory, entry_add_file, entry_custom_cover)
             showinfo("Window", "Conversion successful!")
         except TypeError:
-            showinfo("Window", 'Entry 1 or Entry 2 not filled in')
+            showinfo("Window", 'Add directory and/or Add file not filled in')
         except FileNotFoundError:
             showinfo("Window", "No such directory " + entry_add_directory)
         except OSError:
