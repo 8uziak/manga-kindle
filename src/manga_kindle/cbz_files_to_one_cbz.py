a755dbac68a12d72dffd6fc5ec42d7a558b8ef05
8uziak <matbuziak@gmail.com>
2024-05-09 12:35:10 +0200

    fixed a bug, documentation fix, code refactored

--- src/manga_kindle/cbz_files_to_one_cbz.py
+++ src/manga_kindle/cbz_files_to_one_cbz.py
@@ -1,11 +1,53 @@
+import copy
 import os
 import re
 import zipfile
 import shutil
 
 class CbzFilesToOneCbz:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, folder_path: str, manga_name: str, custom_cover_path: str | None) -> None:
+        self.folder_path = folder_path
+        self.manga_name = manga_name
+        self.custom_cover_path = custom_cover_path
+
+        self.manga_name_folder = os.path.join(folder_path, manga_name)
+        self.copy_cbz_files = os.path.join(self.manga_name_folder, 'cbz_copy_file')
+        self.processing_folder = os.path.join(self.manga_name_folder, 'processing')
+        self.final_folder = os.path.join(self.manga_name_folder, 'final')
+
+        self.create_folders_to_process_operation(self.manga_name_folder, self.copy_cbz_files, self.processing_folder, self.final_folder)
+        self.move_files_from_source_dir_to_temporary_folder(self.folder_path, self.copy_cbz_files)
+        self.rename_cbz_to_zip(self.copy_cbz_files, self.processing_folder, self.final_folder)
+        self.list_files(self.manga_name_folder)
+
+        shutil.rmtree(self.copy_cbz_files)
+
+        if self.custom_cover_path:
+            self.custom_cover(self.custom_cover_path, self.final_folder)
+            
+        self.final_folder_to_manga_name_folder(folder_path, manga_name)
+
+    def create_folders_to_process_operation(self, manga_name_folder: str | None, copy_cbz_files: str, processing_folder: str, final_folder: str) -> None:
+        if not os.path.exists(manga_name_folder):
+            os.mkdir(manga_name_folder)
+            print(f"Created folder: {manga_name_folder}")
+            
+        os.mkdir(copy_cbz_files)
+        print(f"Created folder: {copy_cbz_files}")
+
+        os.mkdir(processing_folder)
+        print(f"Created folder: {processing_folder}")
+
+        os.mkdir(final_folder)
+        print(f"Created folder: {final_folder}")
+
+    def move_files_from_source_dir_to_temporary_folder(self, folder_path: str, copy_cbz_files: str) -> None:
+        files = os.listdir(folder_path)
+        for file in files:
+            if file.endswith('.cbz'):
+                letsgo = os.path.join(folder_path, file)
+                copy_cbz_files_l = os.path.join(copy_cbz_files, file)
+                self.copy_file_from_one_destination_to_other_destination(letsgo, copy_cbz_files_l)
 
     def list_files(self, directory: str) -> None:
         try:
@@ -19,7 +61,7 @@ class CbzFilesToOneCbz:
     def natural_sort_key(self, s: str) -> str:
         return [int(text) if text.isdigit() else text.lower() for text in re.split(r'(\d+)', s)]
 
-    def copy_file(self, source_file: str, destination_file: str) -> None:
+    def copy_file_from_one_destination_to_other_destination(self, source_file: str, destination_file: str) -> None:
         try:
             shutil.copyfile(source_file, destination_file)
             print(f"File '{source_file}' copied to '{destination_file}' successfully.")
@@ -27,9 +69,8 @@ class CbzFilesToOneCbz:
             print(f"Error occurred: {e}")
 
     def move_files(self, source_folder: str, destination_folder: str) -> None:
-        # List all files in the source folder
         files = os.listdir(source_folder)
-        # Move each file to the destination folder
+
         for file in files:
             source_file_path = os.path.join(source_folder, file)
             destination_file_path = os.path.join(destination_folder, file)
@@ -38,62 +79,37 @@ class CbzFilesToOneCbz:
 
 
     def count_files_in_folder(self, folder_path: str) -> int:
-        # List all files in the folder
         files = os.listdir(folder_path)
-        # Count the number of files
         num_files = len(files)
         return num_files
 
     def final_folder_to_manga_name_folder(self, folder_path: str, manga_name: str) -> None:
-        # Create a folder named "Chainsaw man" if it doesn't exist
         manga_name_folder = os.path.join(folder_path, manga_name)
         final_folder = os.path.join(manga_name_folder, 'final')
-        # Rename final_folder to manga_name_folder    
+
         end_folder_name = os.path.join(manga_name_folder, manga_name)
         os.rename(final_folder, end_folder_name)
         print(f"Renamed {final_folder} to {end_folder_name}")
 
-        # Zip manga_name_folder
         shutil.make_archive(end_folder_name, 'zip', root_dir=folder_path, base_dir=manga_name)
 
-        # Rename the zip file to .cbz
         os.rename(f"{end_folder_name}.zip", f"{end_folder_name}.cbz")
         print(f"Renamed {end_folder_name}.zip to {end_folder_name}.cbz")
     
     def custom_cover(self, custom_cover_path: str, final_folder: str) -> None:
         destination_file = os.path.join(final_folder, os.path.basename(custom_cover_path))
-        self.copy_file(custom_cover_path, destination_file)
+        self.copy_file_from_one_destination_to_other_destination(custom_cover_path, destination_file)
 
-    def rename_cbz_to_zip(self, folder_path: str, manga_name: str, custom_cover_path=None) -> None:
-        manga_name_folder = os.path.join(folder_path, manga_name)
-        processing_folder = os.path.join(manga_name_folder, 'processing')
-        final_folder = os.path.join(manga_name_folder, 'final')
-        if not os.path.exists(manga_name_folder):
-            os.mkdir(manga_name_folder)
-            print(f"Created folder: {manga_name_folder}")
-            # Create a folder inside this folder
-            os.mkdir(processing_folder)
-            print(f"Created folder: {processing_folder}")
-            # Create a folder inside this folder
-            os.mkdir(final_folder)
-            print(f"Created folder: {final_folder}")
-        
-        # List all files in the folder_path
-        files = sorted(os.listdir(folder_path), key=self.natural_sort_key)
-        print(files)
-        
-        # Iterate over each file
+    def rename_cbz_to_zip(self, copy_cbz_files: str, processing_folder: str, final_folder: str) -> None:
+        files = sorted(os.listdir(copy_cbz_files), key=self.natural_sort_key)
         for file in files:
             if file.endswith('.cbz'):
-                # Construct old and new file paths
-                old_file_path = os.path.join(folder_path, file)
-                new_file_path = os.path.join(folder_path, file.replace('.cbz', '.zip'))
+                old_file_path = os.path.join(copy_cbz_files, file)
+                new_file_path = os.path.join(copy_cbz_files, file.replace('.cbz', '.zip'))
                 
-                # Rename the file
                 os.rename(old_file_path, new_file_path)
                 print(f"Renamed {old_file_path} to {new_file_path}")
 
-                # Unzip the file
                 with zipfile.ZipFile(new_file_path, 'r') as zip_ref:
                     zip_ref.extractall(processing_folder)
                     print(f"Unzipped {new_file_path}")
@@ -114,15 +130,8 @@ class CbzFilesToOneCbz:
                     os.rename(old_page_path, new_page_path)
                     print(f"Renamed {old_page_path} to {new_page_path}")
 
-                # Call the function to move files
                 self.move_files(processing_folder, final_folder)
 
                 shutil.rmtree(processing_folder)
+                
                 print(f"Deleted all files from {processing_folder}")
-        
-        self.list_files(manga_name_folder)
-
-        if custom_cover_path:
-            self.custom_cover(custom_cover_path, final_folder)
-            
-        self.final_folder_to_manga_name_folder(folder_path, manga_name)
